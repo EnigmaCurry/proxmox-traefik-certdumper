04bd1543e87f553e42b6358d9a9cdd41f300aaf6
Ryan McGuire <ryan@enigmacurry.com>
2019-06-08 15:07:59 -0400

    fix path

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ set -e
 
 function dump() {
     bash dumpcerts.sh /traefik/acme.json /output/ssl
-    for crt_file in $(ls /output/certs/*); do
+    for crt_file in $(ls /output/ssl/certs/*); do
         pem_file=$(echo $crt_file | sed 's/certs/pem/g' | sed 's/.crt/-public.pem/g')
         echo "openssl x509 -inform PEM -in $crt_file > $pem_file"
         openssl x509 -inform PEM -in $crt_file > $pem_file
     done 
-    for key_file in $(ls /output/private/*); do
+    for key_file in $(ls /output/ssl/private/*); do
         pem_file=$(echo $key_file | sed 's/private/pem/g' | sed 's/.key/-private.pem/g')
         echo "openssl rsa -in $key_file -text > $pem_file"
         openssl rsa -in $key_file -text > $pem_file
