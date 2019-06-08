f49e6d6ba2ef8ce17fdb1d98f1e050058e6f3d3b
Ryan McGuire <ryan@enigmacurry.com>
2019-06-08 14:35:05 -0400

    avoid symlinks

--- run_default.sh
+++ run_default.sh
@@ -3,8 +3,8 @@ set -e
 
 function dump() {
     bash dumpcerts.sh /traefik/acme.json /output/
-    ln -f /output/certs/* /output/
-    ln -f /output/private/* /output/
+    #ln -f /output/certs/* /output/
+    #ln -f /output/private/* /output/
     for crt_file in $(ls /output/certs/*); do
         pem_file=$(echo $crt_file | sed 's/certs/pem/g' | sed 's/.crt/-public.pem/g')
         echo "openssl x509 -inform PEM -in $crt_file > $pem_file"
